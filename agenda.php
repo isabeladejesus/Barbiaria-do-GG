52ca76c18bcbd791a6b7d67572dcd8891f03ac08
isabeladejesus <dejesusrodriguesisabela@gmail.com>
2024-10-30 15:29:45 -0300

    agenda add

--- agenda.php
+++ agenda.php
@@ -19,9 +19,20 @@ echo "<h1>Geração de agendamentos do primeiro horário!</h1>";
 for($i = 0; $i < $qnt_de_agendamento; $i++){
 
     echo "\n Criado agendamento no horario para: " . date('H:i', $acumulado);
+    incluirAgenda($dia,date('H:i',$acumulado), $id_usuario);
     $acumulado = $acumulado + 1800;
 }
 
+
+function incluirAgenda($dia,$horario,$id_usuario){
+$sql_agenda = "INSET INTO agenda (dia, horario, id_usuario);
+VALUES ($dia, '$horario', $id_usuario)";
+if ($conn->query($sql_agenda) === TRUE) {
+        return true;
+    }else {
+        echo "Error: ". $sql_usuario . "<br>" . $conn->error;
+    }
+}
 
 
 
@@ -29,7 +40,7 @@ $inicio = strtotime($_POST['horario_volta_intervalo']);
 $final = strtotime($_POST['horario_fim']);
 $mins = ($inicio - $final) / 60;
 
-if($mins < 0)
+if($mins < 0);
     $mins = $mins*(-1);
 //echo $mins;
 
